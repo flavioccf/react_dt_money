f5aab11bd07122240709a025ae9ef825ab9489cc
flavioccf <flavioccf@hotmail.com>
2024-06-17 21:49:30 -0700

    adjust bgColor to be optional

--- src/components/Summary/SummaryCard/index.tsx
+++ src/components/Summary/SummaryCard/index.tsx
@@ -2,17 +2,18 @@ interface SummaryCardProps {
   icon: JSX.Element;
   title: string;
   value: string;
-  bgColor: string;
+  bgColor?: string;
 }
 
-export function SummaryCard({ ...props }: SummaryCardProps) {
+export function SummaryCard({ icon, title, value, bgColor }: SummaryCardProps) {
+  const bkgColor = bgColor || "bg-gray-600";
   return (
-    <div className={`${props.bgColor} rounded-md p-8`}>
+    <div className={`${bkgColor} rounded-md p-8`}>
       <header className="flex items-center justify-between text-gray-300">
-        <span>{props.title}</span>
-        {props.icon}
+        <span>{title}</span>
+        {icon}
       </header>
-      <strong className="block mt-4 text-3xl">{props.value}</strong>
+      <strong className="block mt-4 text-3xl">{value}</strong>
     </div>
   );
 }
