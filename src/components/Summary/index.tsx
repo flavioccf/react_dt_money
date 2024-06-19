59ef0965f7431e2e7960a69cc7d307a43b31dac9
flaviocc <flaviocc@hotmail.com>
2024-06-18 23:00:16 -0700

    add table and search form

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -3,15 +3,15 @@ import { SummaryCard } from "./SummaryCard";
 
 export function Summary() {
   return (
-    <section className="w-full max-w-screen-lg px-6 my-0 mx-auto justify-between items-center grid grid-cols-3 gap-8 -mt-20 text-white">
+    <section className="w-full max-w-screen-lg px-6 my-0 mx-auto justify-between items-center grid grid-cols-3 gap-8 -mt-20">
       <SummaryCard
         icon={<ArrowCircleUp size={32} color="#00b37e" />}
-        title="Deposits"
+        title="Income"
         value="$ 17,400.00"
       />
       <SummaryCard
         icon={<ArrowCircleDown size={32} color="#f75a68" />}
-        title="Withdraws"
+        title="Outcome"
         value="$ 17,400.00"
       />
       <SummaryCard
--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,49 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { SearchForm } from "./components/SearchForm";
 
 export function Transactions() {
   return (
     <div>
       <Header />
       <Summary></Summary>
+      <main className="w-full max-w-screen-lg px-6 mt-16 mb-0 mx-auto">
+        <SearchForm />
+        <table className="w-full border-separate border-spacing-x-0 border-spacing-y-2 mt-6">
+          <tbody>
+            <tr>
+              <td
+                className="py-8 px-5 bg-gray-700 rounded-tl-md rounded-bl-md"
+                width="50%"
+              >
+                Website Development
+              </td>
+              <td className="py-8 px-5 bg-gray-700">
+                <span className="text-green-300">$12,000.00</span>
+              </td>
+              <td className="py-8 px-5 bg-gray-700">Contract</td>
+              <td className="py-8 px-5 bg-gray-700 rounded-tr-md rounded-br-md">
+                04/13/2024
+              </td>
+            </tr>
+            <tr>
+              <td
+                className="py-8 px-5 bg-gray-700 rounded-tl-md rounded-bl-md"
+                width="50%"
+              >
+                Hydro Bill
+              </td>
+              <td className="py-8 px-5 bg-gray-700">
+                <span className="text-red-300">- $2,000.00</span>
+              </td>
+              <td className="py-8 px-5 bg-gray-700">Bill</td>
+              <td className="py-8 px-5 bg-gray-700 rounded-tr-md rounded-br-md">
+                04/17/2024
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </main>
     </div>
   );
 }
