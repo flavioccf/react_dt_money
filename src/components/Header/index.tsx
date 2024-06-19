89d34a1571f076805038eb5bd831df852fc51b2b
flavioccf <flavioccf@hotmail.com>
2024-06-18 23:00:16 -0700

    add table and search form

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,7 @@
 import logoImg from "../../assets/logo.svg";
 export function Header() {
   return (
-    <header className="pt-10 px-0 pb-28 bg-gray-900 text-white">
+    <header className="pt-10 px-0 pb-28 bg-gray-900">
       <div className="w-full max-w-screen-lg px-6 my-0 mx-auto flex justify-between items-center">
         <span className="flex justify-center items-center gap-4">
           <img src={logoImg} alt="" />
---
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -0,0 +1,19 @@
+import { MagnifyingGlass } from "phosphor-react";
+
+export function SearchForm() {
+  return (
+    <form className="flex gap-4">
+      <input
+        className="rounded-md border-0 flex-1 bg-gray-900 text-gray-300 p-4 placeholder:text-gray-500 focus:outline-0 focus:shadow-[0_0_0_2px] focus:shadow-green-500 focus-visible:shadow-green-500"
+        placeholder="Search by transaction"
+      />
+      <button
+        className="flex items-center justify-center gap-3 border-2 border-green-300 text-green-300 font-bold p-4 bg-transparent rounded-md hover:bg-green-500 hover:border-green-500 hover:text-white transition"
+        type="submit"
+      >
+        <MagnifyingGlass size={20} />
+        Search
+      </button>
+    </form>
+  );
+}
